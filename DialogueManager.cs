b1da9d6a055e24dc89277d67106073f7f5d23529
misaacs21 <32146041+misaacs21@users.noreply.github.com>
2019-10-10 14:00:02 -0400

    added additional functionality
    can now have multiple lines of dialogue per branch

--- DialogueManager.cs
+++ DialogueManager.cs
@@ -18,7 +18,8 @@ public class DialogueManager : MonoBehaviour
     public Text TextBox; // the text body
     public Text NameText; // the text body of the name you want to display
     public bool freezePlayerOnDialogue = true;
-
+	private bool enteredY = false;
+	private bool enteredN = false;
     // private bool isOpen; // represents if the dialogue box is open or closed
 
     private Queue<string> inputStream = new Queue<string>(); // stores dialogue
@@ -61,37 +62,53 @@ public class DialogueManager : MonoBehaviour
 
     private void PrintDialogue()
     {
-		Debug.Log("print:" + inputStream.Peek());
         if (inputStream.Peek().Contains("EndQueue")) // special phrase to stop dialogue
         {
             inputStream.Dequeue(); // Clear Queue
             EndDialogue();
         }
+		else if (inputStream.Peek().Contains("EndBranch")) 
+		{
+			enteredY = false;
+			enteredN = false;
+			inputStream.Dequeue();
+			PrintDialogue();
+		}
         else if (inputStream.Peek().Contains("[NAME="))
         {
             string name = inputStream.Peek();
             name = inputStream.Dequeue().Substring(name.IndexOf('=') + 1, name.IndexOf(']') - (name.IndexOf('=') + 1));
             NameText.text = name;
-			Debug.Log("after name:" + inputStream.Peek());
             PrintDialogue(); // print the rest of this line
         }
-		else if (inputStream.Peek().Contains("{C}"))
+		else if (inputStream.Peek().Contains("{Y}") && Input.GetKey(KeyCode.C)) //Prints branch of dialogue entered by pressing C. MUST have {Y} first in dialogue document.
+		{ 
+			enteredY = true;
+			string text = inputStream.Peek();
+			text = inputStream.Dequeue().Substring(text.IndexOf('}')+1);
+			TextBox.text = text;
+		}
+		else if (inputStream.Peek().Contains("{Y}") && enteredY) //continues Y branch
 		{
 			string text = inputStream.Peek();
-			text = inputStream.Dequeue().Substring(text.IndexOf('}') +1);
+			text = inputStream.Dequeue().Substring(text.IndexOf('}')+1);
 			TextBox.text = text;
 		}
-		else if (inputStream.Peek().Contains("{Y}") && Input.GetKey(KeyCode.C)) {
+		else if (inputStream.Peek().Contains("{N}") && Input.GetKey(KeyCode.X) && !enteredY) //Prints branch of dialogue entered by pressing X, only if have not already picked {Y}
+		{ 
+			enteredN = true;
 			string text = inputStream.Peek();
 			text = inputStream.Dequeue().Substring(text.IndexOf('}')+1);
 			TextBox.text = text;
 		}
-		else if (inputStream.Peek().Contains("{N}") && Input.GetKey(KeyCode.X)) {
+		else if (inputStream.Peek().Contains("{N}") && enteredN && !enteredY) //continues N branch 
+		{
 			string text = inputStream.Peek();
 			text = inputStream.Dequeue().Substring(text.IndexOf('}')+1);
 			TextBox.text = text;
 		}
-		else if (inputStream.Peek().Contains("{")) {
+		
+		else if (inputStream.Peek().Contains("{")) { //catches any stray N branch element and removes it before it gets to default
 			inputStream.Dequeue();
 			PrintDialogue();
 		}
